a85ebcd362a7df7558b0cb0d6bffde5fa2b583fc
gso1910 <gustav.bj@gmail.com>
2019-06-07 22:30:52 +0200

    Outline / sketch of procedure

--- src/app/ParseScheme.java
+++ src/app/ParseScheme.java
@@ -3,6 +3,22 @@ package app;
 public class ParseScheme {
 
     public static void main(String[] args) {
-	
+        // Get filename from parameter: file to process
+        // Get filename from parameter: output file
+
+        // Find pattern: value="######"
+        // Replace with: value="C1C1C1"
+
+        // Steps:
+        // - Open the files
+        // - Read next line of the input file
+        // - Use regex pattern to select a color value setting
+        // - Replace the color value setting, if found
+        // - Write the new (or unchanged) line to the output file
+        // - Repeat until every line has been processed
+
+        // The parser will not be able to differentiate between background
+        // and foreground color settings
+
     }
 }
